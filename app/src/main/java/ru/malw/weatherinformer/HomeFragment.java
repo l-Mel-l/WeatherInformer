02ab6c3f17acffc1a5ec84ea647fcf3ff755ff1e
ImMALWARE <notmalware@inbox.ru>
2023-12-02 21:43:37 +0300

    Отображение дат + оптимизация refresh

--- app/src/main/java/ru/malw/weatherinformer/HomeFragment.java
+++ app/src/main/java/ru/malw/weatherinformer/HomeFragment.java
@@ -2,6 +2,8 @@ package ru.malw.weatherinformer;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,14 +16,15 @@ import android.widget.TextView;
 import androidx.appcompat.widget.TooltipCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.weatherinformer.R;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
@@ -34,7 +37,7 @@ public class HomeFragment extends Fragment {
         root = inflater.inflate(R.layout.fragment_home, container, false);
         refresh(getContext());
         return root;
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+//        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 
     private void refresh(Context context) {
@@ -47,44 +50,43 @@ public class HomeFragment extends Fragment {
 
                 getActivity().runOnUiThread(() -> {
                     try {
+                        ((TextView) root.findViewById(R.id.TemperatureText)).setText(String.valueOf(Math.round(Double.parseDouble(weather.getJSONArray("list").getJSONObject(0).getJSONObject("main").getString("temp")))));
+                        String description = weather.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0).getString("description");
+                        ((TextView) root.findViewById(R.id.WeatherText)).setText(description.substring(0, 1).toUpperCase() + description.substring(1));
+                        ((TextView) root.findViewById(R.id.FeelsLikeText)).setText("Ощущается как " + Math.round(Double.parseDouble(weather.getJSONArray("list").getJSONObject(0).getJSONObject("main").getString("feels_like"))));
+                        String units = Data.UseFahrenheit ? "F" : "C";
                         for (int e = 1; e <= 40; e++) {
-                            int textViewId = getResources().getIdentifier("t" + e, "id", requireActivity().getPackageName());
-                            int imageViewId = getResources().getIdentifier("i" + e, "id", requireActivity().getPackageName());
-
-                            TextView t = root.findViewById(textViewId);
-                            ImageView i = root.findViewById(imageViewId);
-
+                            TextView t = root.findViewById(getResources().getIdentifier("t" + e, "id", requireActivity().getPackageName()));
+                            ImageView i = root.findViewById(getResources().getIdentifier("i" + e, "id", requireActivity().getPackageName()));
                             try {
-                                double temperature = weather.getJSONArray("list").getJSONObject(e).getJSONObject("main").getDouble("temp");
-                                int roundedTemperature = (int) Math.round(temperature);
-                                t.setText(roundedTemperature + "°");
-                                String icon = "i"+weather.getJSONArray("list").getJSONObject(e).getJSONArray("weather").getJSONObject(0).getString("icon").substring(0, 2);
-                                int iconResourceId = getResources().getIdentifier(icon, "drawable", requireActivity().getPackageName());
-                                i.setImageResource(iconResourceId);
-                                i.setContentDescription(weather.getJSONArray("list").getJSONObject(e).getJSONArray("weather").getJSONObject(0).getString("description"));
-                                TooltipCompat.setTooltipText(i, weather.getJSONArray("list").getJSONObject(e).getJSONArray("weather").getJSONObject(0).getString("description"));
-                            } catch (Exception ex) {
+                                t.setText(Math.round(weather.getJSONArray("list").getJSONObject(e).getJSONObject("main").getDouble("temp")) + "°" + units);
+                                i.setImageResource(getResources().getIdentifier("i"+weather.getJSONArray("list").getJSONObject(e).getJSONArray("weather").getJSONObject(0).getString("icon").substring(0, 2), "drawable", requireActivity().getPackageName()));
+                                description = weather.getJSONArray("list").getJSONObject(e).getJSONArray("weather").getJSONObject(0).getString("description");
+                                TooltipCompat.setTooltipText(i, description.substring(0, 1).toUpperCase() + description.substring(1));
+                            } catch (JSONException ex) {
                                 t.setText("???");
-                                ex.printStackTrace();
                                 i.setImageResource(R.drawable.unknown);
-                                //i.setContentDescription(getString(R.string.unknown));
+                                TooltipCompat.setTooltipText(i, "Неизвестно");
                             }
                         }
-                        ((TextView) root.findViewById(R.id.TemperatureText)).setText(String.valueOf(Math.round(Double.parseDouble(weather.getJSONArray("list").getJSONObject(0).getJSONObject("main").getString("temp")))));
-                        ((TextView) root.findViewById(R.id.WeatherText)).setText(weather.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0).getString("description"));
-                        ((TextView) root.findViewById(R.id.FeelsLikeText)).setText("Ощущается как " + Math.round(Double.parseDouble(weather.getJSONArray("list").getJSONObject(0).getJSONObject("main").getString("feels_like"))));
+                        for (int d : new int[]{ 9, 17, 25, 33}) {
+                            String date = new SimpleDateFormat("EEEE, d MMMM", Locale.forLanguageTag("ru")).format(new Date(Long.parseLong(weather.getJSONArray("list").getJSONObject(d).getString("dt")) * 1000));
+                            ((TextView) root.findViewById(getResources().getIdentifier("d" + d, "id", requireActivity().getPackageName()))).setText(date.substring(0, 1).toUpperCase() + date.substring(1));
+                        }
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
                     }
                 });
             } catch (IOException | JSONException e) {
                 new Handler(Looper.getMainLooper()).post(() ->
-                        // TODO: надо будет потом сделать диалоговое окно об ошибке таким же, как в десктопе. Но щас лень.
                         new AlertDialog.Builder(context)
-                            .setTitle("Не удалось получить данные с сервера!")
-                            .setMessage("Контакты не были синхронизированы с сервером!")
-                            .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
-                            .show()
+                                .setTitle("Ошибка отправки запроса!")
+                                .setMessage("Попробуйте сменить IP адрес (перезагрузить роутер или использовать VPN). Показать рекомендуемый VPN-сервис?")
+                                .setPositiveButton("Да", (dialog, which) -> {
+                                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://zelenka.guru/threads/4807721")));
+                                    dialog.dismiss();})
+                                .setNegativeButton("Нет", null)
+                                .show()
                 );
             }
         }, Executors.newSingleThreadExecutor()).thenRun(() -> Executors.newSingleThreadExecutor().shutdown());
